e5d5b7c0b29a30a2564396ec3e1855ad83b355c2
morettiale <alex.moretti22@gmail.com>
2024-09-19 13:03:47 +0200

    added method in service

--- loans/src/main/java/eu/proximagroup/loans/services/LoanService.java
+++ loans/src/main/java/eu/proximagroup/loans/services/LoanService.java
@@ -1,11 +1,10 @@
-package eu.proximagroup.services;
+package eu.proximagroup.loans.services;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import eu.proximagroup.models.Loan;
-import eu.proximagroup.repositories.LoanRepository;
+import eu.proximagroup.loans.exceptions.ResourceNotFoundException;
+import eu.proximagroup.loans.models.Loan;
+import eu.proximagroup.loans.repositories.LoanRepository;
 
 @Service
 public class LoanService {
@@ -18,5 +17,30 @@ public class LoanService {
 	
 	public List<Loan> getAll(){
 		return loanRepository.findAll();
+	}
+	
+	public Loan getById(Long id) {
+		isPresent(id);
+		Loan loan=this.loanRepository.findById(id).get();
+		return loan;
+	}
+	
+	public Loan store(Loan loan) {
+		return this.loanRepository.save(loan);
+	}
+	
+	public Loan update(Loan loan,Long id) {
+		loan.setId(id);
+		return this.loanRepository.save(loan);
+	}
+	
+	public void deleteById(Long id) {
+		isPresent(id);
+		this.loanRepository.deleteById(id);
+	}
+	
+	public void isPresent(Long id) {
+		this.loanRepository.findById(id)
+			.orElseThrow( () -> new ResourceNotFoundException("Loan", "id", id.toString()));
 	}
 }
