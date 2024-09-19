a75241ab8a0fc112edcf69750b0fbe11facd7884
morettiale <alex.moretti22@gmail.com>
2024-09-19 13:00:34 +0200

    bug fix

--- accounts/src/main/java/eu/proximagroup/accounts/exceptions/ResourceNotFoundException.java
+++ accounts/src/main/java/eu/proximagroup/accounts/exceptions/ResourceNotFoundException.java
@@ -3,6 +3,11 @@ package eu.proximagroup.accounts.exceptions;
 //utilizzo runtime perché così creo un'eccezione che genera errore nel momento in cui lancio il programma
 public class ResourceNotFoundException extends RuntimeException{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
         super(String.format("%s not found with the given input data %s : '%s'", resourceName, fieldName, fieldValue));
     }
