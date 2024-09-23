aaa8020864d3ebd1f6633b3d184cc72f9636692c
morettiale <alex.moretti22@gmail.com>
2024-09-23 10:48:12 +0200

    created info controller in account

--- accounts/src/main/java/eu/proximagroup/accounts/controllers/InfoController.java
+++ accounts/src/main/java/eu/proximagroup/accounts/controllers/InfoController.java
@@ -1,5 +1,8 @@
 package eu.proximagroup.accounts.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class InfoController {
 
+	@Autowired
+	private Environment envirorment;
+	
+	@Value("${build.version}")
+	private String buildVersion;
+	
+	
 	@GetMapping("/test")
 	public ResponseEntity<?> test() {
 		return ResponseEntity.status(HttpStatus.OK).body("Sta correndo");
+	}
+	
+	@GetMapping("/version")
+	public ResponseEntity<String> buildVersion(){
+		return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+	}
+	
+	@GetMapping("/env")
+	public ResponseEntity<String> env(){
+		return ResponseEntity.status(HttpStatus.OK).body(envirorment.getProperty("ambiente"));
 	}
 }
